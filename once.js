3792d67de50f938230ad9ff4d2a1bd5de260e38d
paztis-akshay <82502149+paztis-akshay@users.noreply.github.com>
2021-04-14 18:29:03 +0530

    Create once.js
    Implementation for once lodash function

---
+++ once.js
@@ -0,0 +1,20 @@
+function once(func) {
+    let result;
+    return function(a, b) {
+        if(func) {
+            result = func.apply(this, arguments);
+            func = null;
+        }
+        return result;
+    }
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+var onceAdd = once(add);
+console.log(onceAdd(1,2));
+console.log(onceAdd(7,2));
+console.log(onceAdd(1,5));
+console.log(onceAdd(9,2));
